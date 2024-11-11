8fd61d2267dd44d8767acaf56375bf6c08879c6a
TruongChiBao <chibaotruong1506@gmail.com>
2024-11-11 21:55:33 +0700

    get event

--- src/controllers/eventController.js
+++ src/controllers/eventController.js
@@ -6,8 +6,9 @@ const addNewEvent = asyncHandler(async (req, res) => {
     const body = req.body;
     if(body){
         const newEvent = new EventModel(body);
-        newEvent.save();
         console.log(newEvent);
+        newEvent.save();
+        
         
         res.status(200).json({
             message: 'Added new event successfully',
@@ -20,4 +21,15 @@ const addNewEvent = asyncHandler(async (req, res) => {
     
 });
 
-module.exports = {addNewEvent};
+const getEvents = asyncHandler(async (req, res)=>{
+    const {lat, lon, distance,limit} = req.query;
+    const events = await EventModel.find({}).sort({ createdAt: -1 }).limit(limit ?? 0);
+    
+    res.status(200).json({
+        message:'get Events Ok',
+        data:events,
+    });
+    
+});
+
+module.exports = {addNewEvent, getEvents};
--- src/routers/eventRouter.js
+++ src/routers/eventRouter.js
@@ -1,9 +1,10 @@
 
 const Router = require('express');
-const { addNewEvent } = require('../controllers/eventController');
+const { addNewEvent, getEvents } = require('../controllers/eventController');
 
 const eventRouter = Router();
 
 eventRouter.post('/add-new', addNewEvent);
+eventRouter.get('/get-events', getEvents)
 
 module.exports = eventRouter;
